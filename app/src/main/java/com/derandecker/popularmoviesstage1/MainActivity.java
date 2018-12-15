1dd6863487795105b517e26ce627b68e3412021f
Deran Decker <me@derandecker.com>
2018-12-15 01:32:14 -1000

    Add network error handling code

--- app/src/main/java/com/derandecker/popularmoviesstage1/MainActivity.java
+++ app/src/main/java/com/derandecker/popularmoviesstage1/MainActivity.java
@@ -1,5 +1,8 @@
 package com.derandecker.popularmoviesstage1;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -46,9 +49,11 @@ public class MainActivity extends AppCompatActivity {
         mMoviesPics.setAdapter(mMovie);
     }
 
-    private void closeOnError() {
-        finish();
-        Toast.makeText(this, "No movie data", Toast.LENGTH_SHORT).show();
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 
     public class TmdbMovieTask extends AsyncTask<URL, Void, String> {
@@ -57,18 +62,26 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(URL... params) {
             String movieString;
             URL url = params[0];
-            try {
-                movieString = NetworkUtils.getResponseFromHttpUrl(url);
-                return movieString;
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (isOnline()) {
+                try {
+                    movieString = NetworkUtils.getResponseFromHttpUrl(url);
+                    return movieString;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
             }
             return null;
         }
 
         @Override
         protected void onPostExecute(String tmdbMovies) {
-            populateUI(tmdbMovies);
+            if (tmdbMovies != null) {
+                populateUI(tmdbMovies);
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Network error", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
